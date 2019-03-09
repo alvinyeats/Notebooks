edd317e67c5e0ba8b0737277d37d57417218d33b
alvin yeats <alvinyeats@users.noreply.github.com>
2019-03-09 23:00:18 +0800

    Update test.sh

--- shell/test.sh
+++ shell/test.sh
@@ -1,9 +1,6 @@
-echo ""
-echo "#############################################################"
-echo "#Centos 系统初始化"
-echo "# Intro:"
-echo "#"
-echo "# Author: tommy sun"
-echo "#"
-echo "#############################################################"
-echo ""
+# !/bin/bash                                 # 指定shell类型
+
+echo -n "Enter your name:"                   # 参数-n的作用是不换行，echo默认换行
+read  name                                   # 把键盘输入放入变量name
+echo "hello $name,welcome to my program"     # 显示输入信息
+exit 0                                       # 返回一个零退出状态，退出shell程序
